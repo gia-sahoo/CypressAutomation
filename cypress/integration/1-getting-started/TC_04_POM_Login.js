2bb0a03bdcd4380721cd83aac3bface0176124cf
gia-sahoo <tapas.ku.sahoo@gmail.com>
2022-01-26 23:20:20 -0600

    test POM fail

--- cypress/integration/1-getting-started/TC_04_POM_Login.js
+++ cypress/integration/1-getting-started/TC_04_POM_Login.js
@@ -8,6 +8,6 @@ describe('Login(POMlogin)',{tags:'@POMlogin'}, function(){
         login.email().type('qamilestone.academy@gmail.com')
         login.password().type('admin123')
         login.signInButton().should('be.visible').click()
-        //cy.get('input[type="password5"]').should('be.visible')//expect to fail
+        cy.get('input[type="password5"]').should('be.visible')//expect to fail
     })
 })
